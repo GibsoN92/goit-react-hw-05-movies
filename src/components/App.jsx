d12c9add2c253d8fa383e21a2beeea6415388a48
GibsoN92 <52374403+GibsoN92@users.noreply.github.com>
2024-06-30 20:17:56 +0200

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,28 @@
+import "./App.scss";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { lazy } from "react";
+import SharedLayout from "./SharedLayout/SharedLayout";
+
+const Home = lazy(() => import("../Pages/Home"));
+const Movies = lazy(() => import("../Pages/Movies"));
+const MovieDetails = lazy(() => import("./MovieDetails/MovieDetails"));
+const Cast = lazy(() => import("./Cast/Cast"));
+const Reviews = lazy(() => import("./Reviews/Reviews"));
+
 function App() {
   return (
     <>
-      <h1>React Homework Template (Vite)</h1>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />}></Route>
+            <Route path="reviews" element={<Reviews />}></Route>
+          </Route>
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
+      </Routes>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App.jsx";
+import { HashRouter } from "react-router-dom";
+import "./main.scss";
+import App from "./components/App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <HashRouter>
     <App />
-  </React.StrictMode>,
+  </HashRouter>,
 );
